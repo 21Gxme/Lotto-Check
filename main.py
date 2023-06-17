1689bd268f6fc781b70337f78c1cc4e78c86d3a9
Tantikorn Prasanpeangsi <tantikorn@Macintosh-5.local>
2023-06-17 20:57:13 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import tkinter as tk
+
+
+url = "https://www.lottery.co.th/small"
+data = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+soup = BeautifulSoup(data.text, "html.parser")
+loterry = soup.find_all("button", {"class": "btn-primary"})
+loterry_list = []
+for i in loterry:
+    loterry_list.append(i.text)
+print(loterry_list)
+
+root = tk.Tk()
+root.title("Lottery")
+root.geometry("300x200")
+root.resizable(0, 0)
+msg1 = tk.Label(root, text="รางวัลที่ 1", font=("Arial", 20))
+msg1.grid(row=0, column=0, padx=10, pady=10)
+reward = tk.Label(root, text=loterry_list[0], font=("Arial", 20))
+reward.grid(row=0, column=1, padx=10, pady=10)
+msg2 = tk.Label(root, text="เลขท้าย 2 ตัว", font=("Arial", 20))
+msg2.grid(row=1, column=0, padx=10, pady=10)
+reward = tk.Label(root, text=loterry_list[1], font=("Arial", 20))
+reward.grid(row=1, column=1, padx=10, pady=10)
+msg3 = tk.Label(root, text="เลขท้าย 3 ตัว", font=("Arial", 20))
+msg3.grid(row=2, column=0, padx=10, pady=10)
+reward = tk.Label(root, text=loterry_list[2], font=("Arial", 20))
+reward.grid(row=2, column=1, padx=10, pady=10)
+reward = tk.Label(root, text=loterry_list[3], font=("Arial", 20))
+reward.grid(row=2, column=2, padx=10, pady=10)
+msg4 = tk.Label(root, text="เลขหน้า 3 ตัว", font=("Arial", 20))
+msg4.grid(row=3, column=0, padx=10, pady=10)
+reward = tk.Label(root, text=loterry_list[4], font=("Arial", 20))
+reward.grid(row=3, column=1, padx=10, pady=10)
+reward = tk.Label(root, text=loterry_list[5], font=("Arial", 20))
+reward.grid(row=3, column=2, padx=10, pady=10)
+
+root.mainloop()
